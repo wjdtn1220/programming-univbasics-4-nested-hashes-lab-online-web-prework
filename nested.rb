a50838aee602e07ffa1bba31d28866a8073caaa8
Jeong Park <wjdtn1220@gmail.com>
2019-08-02 02:50:41 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -55,7 +55,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-  programmer_hash[:dennis_ritchie][:languages] = string
+  programmer_hash[:dennis_ritchie][:languages] = "string"
 end
 
 def adding_matz
