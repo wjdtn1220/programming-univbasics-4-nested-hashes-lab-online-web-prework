068c874707080427b895b2f8aef4fbca88ff0383
Jeong Park <wjdtn1220@gmail.com>
2019-08-02 02:48:02 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -1,13 +1,22 @@
 
 def hopper
-	programmer_hash = {
-	  :grace_hopper => {
-	    :known_for => "COBOL",
-	    :language => ["C"]
-	  }
-	}
-programmer_hash[:grace_hopper]
-
+	programmer_hash = 
+ 		{
+        :grace_hopper => {
+          :known_for => "COBOL",
+          :languages => ["COBOL", "FORTRAN"]
+        },
+        :alan_kay => {
+          :known_for => "Object Orientation",
+          :languages => ["Smalltalk", "LISP"]
+        },
+        :dennis_ritchie => {
+          :known_for => "Unix",
+          :languages => ["C"]
+        }
+     }
+  
+  programmer_hash[:]
 end
 
 def alan_kay_is_known_for
