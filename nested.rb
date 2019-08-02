2dc15337d2924c7017f6e7d0fb37886f6d558538
Jeong Park <wjdtn1220@gmail.com>
2019-08-02 02:57:30 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -80,9 +80,9 @@ def adding_matz
           :languages => ["C"]
         }
      }
-  programmer_hash[:hello] = {
-    :known_for => "Hi",
-    :languages => ["C", "LISP"]
+  programmer_hash[:yukihiro_matsumoto] = {
+    :known_for => "Ruby",
+    :languages => ["LISP", "C"]
   }
   programmer_hash
 end
